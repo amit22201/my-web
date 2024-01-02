c3a6e627bfcdf3540c6b114932c868e36bffe646
amit22201 <amitkmr2220@gmail.com>
2024-01-03 00:15:45 +0530

    made home page responsive

--- main.js
+++ main.js
@@ -16,18 +16,16 @@ function setTime(local) {
 
     // Convert hours to 12-hour format and determine AM or PM
     if (format_icon_selected === '12hrFormat') {
-        // console.log(format_icon_selected);
         if (hour > 12) {
             hour -= 12;
             meridiem = 'PM';
-            mer_span.innerText = meridiem
         } else if (hour === 0) {
             hour = 12;
         } else if (hour === 12) {
             meridiem = 'PM';
-            mer_span.innerText = meridiem
         }
     }
+    mer_span.innerText = meridiem
     // Ensure that minutes and seconds always display two digits
     const formattedHour = String(hour).padStart(2, '0');
     const formattedMinutes = String(min).padStart(2, '0');
@@ -52,12 +50,38 @@ formateSelectorIconIcon.addEventListener('click', () => {
 });
 
 formatSelect.addEventListener('change', () => {
+    formateSelectorIcon.classList.toggle('format-selector-icon-selected');
     formatSelect.classList.toggle('format-select-visible');
     format_icon_selected = formatSelect.value;
-    if(format_icon_selected==='24hrFormat'){
-        document.getElementsByClassName('time-section')[3].style.display='none'
-        // document.getElementsByClassName('time-section')[2].style.display='none'
-    } else{
-        document.getElementsByClassName('time-section')[3].style.display='block'
+    if (format_icon_selected === '24hrFormat') {
+        document.getElementsByClassName('time-section')[3].style.display = 'none'
+    } else {
+        document.getElementsByClassName('time-section')[3].style.display = 'block'
+    }
+});
+
+let menuIcon = document.getElementById('menu-icon');
+let headerMenu = document.getElementsByClassName('header-nav').item(0);
+if (window.innerWidth <= 900) {
+    headerMenu.classList.add('disply-none')
+    menuIcon.style.color = 'white';
+}
+menuIcon.addEventListener('click', (event) => {
+    if (headerMenu.classList.toggle('disply-none')) {
+        menuIcon.style.color = 'white';
+    } else {
+        menuIcon.style.color = 'black';
+    }
+});
+
+document.addEventListener('click', (event) => {
+    const containsEvent = menuIcon.contains(event.target);
+    if (!containsEvent && window.innerWidth <= 900) {
+        headerMenu.classList.add('disply-none')
+        menuIcon.style.color = 'white';
+    }
+    if (!formateSelectorIcon.contains(event.target)) {
+        formatSelect.classList.remove('format-select-visible');
+        formateSelectorIcon.classList.remove('format-selector-icon-selected');
     }
 });
